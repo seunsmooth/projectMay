77de6aae7470e61fb0640c52b14b6ee6d348e29c
seun awoga <seunsmooth@yahoo.com>
2020-05-21 18:18:41 +0100

    terraform file update

--- scripts/code_deploy.sh
+++ scripts/code_deploy.sh
@@ -9,9 +9,9 @@ ARTIFACT_API=`packer build -machine-readable packer-ami-nginx-proxy.json  |tee b
 AMI_ID_NGINX=`grep 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2`
 echo 'variable "AMI_ID_NGINX" { default = "'${AMI_ID_NGINX}'" }' > amivar_nginx.tf || true
 
-sleep 2
-
 ARTIFACT_WEB=`packer build -machine-readable packer-ami-web-bluebox.json  |tee build.log `
 AMI_ID_WEB=`grep 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2`
-echo 'variable "AMI_ID_WEB" { default = "'${AMI_ID_WEB}'" }' > amivar_web.tf 
+echo 'variable "AMI_ID_WEB" { default = "'${AMI_ID_WEB}'" }' > amivar_web.tf || true
+
+
 
